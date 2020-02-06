ad14b0832f1985e23b1db2a38c1aeba0b1160c44
Michael Dennis <mdennis281@gmail.com>
2020-02-06 11:08:39 -0600

    Added examples to run.py

--- run.py
+++ run.py
@@ -1,4 +1,24 @@
 from app import Monitor
 
 connectionMonitor = Monitor('8.8.8.8')
+
+'''
+
+other examples:
+connectionMonitor = Monitor(
+	'google.com',
+	output='out.log',
+	testLength=1500  #seconds
+)
+
+connectionMonitor = Monitor(
+	'10.0.0.100',
+	output='outServer.log',
+	pingFrequency=2,  #seconds
+	maxFailCount=0 #number of pings before classified as outage
+)
+
+
+'''
+
 connectionMonitor.start() 
